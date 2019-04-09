8155c9b5b2024ba0eaed0333be967da0e2e21c09
manuk95 <k.nir@hotmail.com>
2019-04-09 19:20:49 +0200

    UserControl

---
+++ TestDrive/UserControl/RunLineView.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestDrive
+{
+    public partial class RunLine : UserControl
+    {
+        public RunLine()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEditAcceleration_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEditSpeed_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
