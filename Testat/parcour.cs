1b4c6ddfcbadf0ee4d3c10ec5e07a7405c678269
manuk95 <k.nir@hotmail.com>
2019-04-16 18:31:40 +0200

    Projekt Testat + Klasse Parcour

---
+++ Testat/parcour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testat
+{
+    class parcour
+    {
+       public static void driveParcour()
+        {
+            //..
+        }
+    }
+
+    
+}
